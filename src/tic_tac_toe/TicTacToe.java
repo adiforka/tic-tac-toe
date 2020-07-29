4fed15c70f7989de5053155975f6329cb401240d
adi forkasiewicz <adiforka@gmail.com>
2020-07-29 10:19:11 +0200

    refactored and developed into a series of games with cleanup ops after every game. now also randomly chooses if player or CPU starts. could implement timer (maybe max 45-60 sec) and summary of no. of games won

--- src/tic_tac_toe/TicTacToe.java
+++ src/tic_tac_toe/TicTacToe.java
@@ -1,142 +1,169 @@
 package tic_tac_toe;
-
 import java.util.*;
+
+import static java.lang.System.*;
 
 public class TicTacToe {
 
-    private static final char[][] gameBoard = {
+    private static char[][] gameboard = {
             {' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '},
     };
-
     private static final List<Integer> playerPositions = new ArrayList<>();
     private static final List<Integer> cpuPositions = new ArrayList<>();
 
 
     public static void main(String[] args) {
 
-        playGame();
+        for (int i = 0; i < 10; i++) {
+            out.printf("GAME NUMBER %d\n", i + 1);
+            playGame();
+        }
     }
 
     public static void playGame() {
-
-        printGameBoard();
         String result;
+        boolean playerTurn = isPlayerTurn();
         while (true) {
-            int playerPos = playerMoves();
-            placePiece(playerPos, "player");
+            if (playerTurn) {
+                printGameBoard();
+                playerMoves();
+            } else {
+                cpuMoves();
+            }
+            playerTurn = !playerTurn;
 
             result = checkWinner();
             if (result.length() > 0) {
-                printResult(result);
+                out.println(result);
                 printGameBoard();
+                cleanUp();
                 break;
             }
 
-            int CPUPos = CPUmoves();
-            placePiece(CPUPos, "cpu");
+            if (playerTurn) {
+                printGameBoard();
+                playerMoves();
+            } else {
+                cpuMoves();
+            }
+            playerTurn = !playerTurn;
 
-            //repetitive code but hard to avoid without complicating further
             result = checkWinner();
             if (result.length() > 0) {
-                printResult(result);
+                out.println(result);
                 printGameBoard();
+                cleanUp();
                 break;
             }
-            //awkward, but seems to be the best of all non-optimal places to print
-            //a board again
-            printGameBoard();
         }
-    }
-
-    private static void printResult(String result) {
-        System.out.println(result);
     }
 
     private static void printGameBoard() {
-        for (char[] row : gameBoard) {
-            for (char c : row) {
-                System.out.print(c);
+        for (char[] row : gameboard) {
+            for (char ch : row) {
+                out.print(ch + " ");
             }
-            System.out.println();
+            out.println();
         }
+        out.println();
     }
 
-    private static int playerMoves() {
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a your placement 1-9: ");
-        int playerPos = scan.nextInt();
-        while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
-            System.out.println("Position taken. Pick a free spot: ");
-            playerPos = scan.nextInt();
-        }
-        return playerPos;
+    private static boolean isPlayerTurn() {
+        int val = new Random().nextInt(2);
+        return val > 0;
     }
 
-    private static int CPUmoves() {
+    private static void playerMoves() {
+        Scanner scanner = new Scanner(in);
+        out.println("Enter placement in a free position 1-9");
+        int position = scanner.nextInt();
+        while (playerPositions.contains(position) || cpuPositions.contains(position)) {
+            out.println("Position taken. Enter placement in a free position 1-9");
+            position = scanner.nextInt();
+        }
 
-        Random random = new Random();
-        int cpuPos;
+        makePlacement(position, "Player");
+    }
+
+    private static void cpuMoves() {
+        var random = new Random(1);
+        int position;
         do {
-            //setting range between 1 and 9 (without the +1) it is 0 - 8
-            cpuPos = random.nextInt(9) + 1;
-        }
-        while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos));
-        return cpuPos;
+            position = random.nextInt(10);
+        } while (cpuPositions.contains(position) || playerPositions.contains(position));
+
+        makePlacement(position, "CPU");
     }
 
-    private static void placePiece(int pos, String user) {
+    private static void makePlacement(int position, String user) {
 
         char placement;
         if (user.equalsIgnoreCase("Player")) {
-            placement = 'O';
-            playerPositions.add(pos);
+            placement = '0';
+            playerPositions.add(position);
         } else {
             placement = 'X';
-            cpuPositions.add(pos);
+            cpuPositions.add(position);
         }
 
-        switch (pos) {
-            case 1 -> gameBoard[0][0] = placement;
-            case 2 -> gameBoard[0][2] = placement;
-            case 3 -> gameBoard[0][4] = placement;
-            case 4 -> gameBoard[2][0] = placement;
-            case 5 -> gameBoard[2][2] = placement;
-            case 6 -> gameBoard[2][4] = placement;
-            case 7 -> gameBoard[4][0] = placement;
-            case 8 -> gameBoard[4][2] = placement;
-            case 9 -> gameBoard[4][4] = placement;
+        switch (position) {
+            case 1 -> gameboard[0][0] = placement;
+            case 2 -> gameboard[0][2] = placement;
+            case 3 -> gameboard[0][4] = placement;
+            case 4 -> gameboard[2][0] = placement;
+            case 5 -> gameboard[2][2] = placement;
+            case 6 -> gameboard[2][4] = placement;
+            case 7 -> gameboard[4][0] = placement;
+            case 8 -> gameboard[4][2] = placement;
+            case 9 -> gameboard[4][4] = placement;
         }
     }
 
     private static String checkWinner() {
+        List<List<Integer>> winningConditions = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8, 9),
+                Arrays.asList(1, 4, 7),
+                Arrays.asList(2, 5, 8),
+                Arrays.asList(3, 6, 9),
+                Arrays.asList(1, 5, 9),
+                Arrays.asList(3, 5, 7)
+        );
 
-        //win conditions
-        List<Integer> topRow = Arrays.asList(1, 2, 3);
-        List<Integer> midRow = Arrays.asList(4, 5, 6);
-        List<Integer> botRow = Arrays.asList(7, 8, 9);
-        List<Integer> leftCol = Arrays.asList(1, 4, 7);
-        List<Integer> midCol = Arrays.asList(2, 5, 8);
-        List<Integer> rightCol = Arrays.asList(3, 6, 9);
-        List<Integer> cross1 = Arrays.asList(1, 5, 9);
-        List<Integer> cross2 = Arrays.asList(3, 5, 7);
-
-        List<List<Integer>> winConditions = Arrays.asList(topRow, midRow, botRow, leftCol,
-                midCol, rightCol, cross1, cross2);
-
-        for (List<Integer> winCondition : winConditions) {
-            if (playerPositions.containsAll(winCondition)) {
+        for (List<Integer> condition : winningConditions) {
+            if (playerPositions.containsAll(condition)) {
                 return "You win!";
-            } else if (cpuPositions.containsAll(winCondition)) {
-                return "CPU wins";
-            } else if (playerPositions.size() + cpuPositions.size() == 9) {
-                return "It's a tie";
+            } else if (cpuPositions.containsAll(condition)) {
+                return "Cpu wins!";
+            } else if (cpuPositions.size() + playerPositions.size() == 9) {
+                return "It's a tie!";
             }
         }
         return "";
+    }
+
+    private static void cleanUp() {
+        restoreGameBoard();
+        clearPositionLists();
+    }
+
+    private static void restoreGameBoard() {
+        gameboard = new char[][]{
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+        };
+    }
+
+    private static void clearPositionLists() {
+        playerPositions.clear();
+        cpuPositions.clear();
     }
 }
